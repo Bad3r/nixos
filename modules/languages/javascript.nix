3107d0da7e34c5703c4c2b9c250fffa16fcf3db1
Bad3r <25513724+Bad3r@users.noreply.github.com>
2025-08-23 08:22:42 +0300

    feat(javascript): add nrm for npm registry management
    - Create nrm wrapper using npx for registry switching
    - Allows easy switching between npm registries\n

--- modules/languages/javascript.nix
+++ modules/languages/javascript.nix
@@ -1,11 +1,17 @@
 {
   flake.modules.nixos.workstation =
     { pkgs, ... }:
+    let
+      nrm = pkgs.writeShellScriptBin "nrm" ''
+        exec ${pkgs.nodejs_22}/bin/npx -y nrm "$@"
+      '';
+    in
     {
       environment.systemPackages = with pkgs; [
         nodejs_22
         nodejs_24
         yarn
+        nrm
       ];
     };
 }
