b3cc683aa112dbd91bd3041d22538c929c04dcb3
Bad3r <25513724+Bad3r@users.noreply.github.com>
2025-09-17 00:15:04 +0300

    fix(audio): add pipewire mixers to system76 host

--- modules/system76/packages.nix
+++ modules/system76/packages.nix
@@ -1,4 +1,5 @@
-_: {
+{ lib, ... }:
+{
   nixpkgs.allowedUnfreePackages = [
     "system76-wallpapers"
     "system76-wallpapers-0-unstable-2024-04-26"
@@ -9,19 +10,22 @@ _: {
   configurations.nixos.system76.module =
     { pkgs, ... }:
     {
-      environment.systemPackages = with pkgs; [
-        # System76 hardware utilities
-        system76-power
-        system76-scheduler
-        system76-firmware
-        system76-wallpapers
-        firmware-manager
-        system76-keyboard-configurator
+      environment.systemPackages = lib.mkAfter (
+        with pkgs;
+        [
+          # System76 hardware utilities
+          system76-power
+          system76-scheduler
+          system76-firmware
+          system76-wallpapers
+          firmware-manager
+          system76-keyboard-configurator
 
-        # PipeWire controls for selecting outputs and adjusting volume
-        pavucontrol
-        qpwgraph
-        helvum
-      ];
+          # PipeWire controls for selecting outputs and adjusting volume
+          pavucontrol
+          qpwgraph
+          helvum
+        ]
+      );
     };
 }
