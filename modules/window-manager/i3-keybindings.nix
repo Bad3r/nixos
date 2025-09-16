10b630c47cb0a80e0fac7c996454e733e3d3ea75
Bad3r <25513724+Bad3r@users.noreply.github.com>
2025-09-16 09:21:06 +0300

    fix:  i3 keybindings

--- modules/window-manager/i3-keybindings.nix
+++ modules/window-manager/i3-keybindings.nix
@@ -1,12 +1,7 @@
 {
   # Home Manager i3 keybindings for common actions
   flake.homeManagerModules.gui =
-    {
-      pkgs,
-      lib,
-      config,
-      ...
-    }:
+    { pkgs, lib, ... }:
     {
       xsession = {
         enable = true;
@@ -17,20 +12,17 @@
             modifier = lib.mkDefault "Mod4";
 
             # Keybindings requested
-            keybindings =
-              let
-                normalizeMap = config.flake.lib.homeManager.i3.normalizeMap or (attrs: attrs);
-              in
-              normalizeMap {
-                # super + Enter = new default terminal window (kitty)
-                "$mod+Return" = "exec ${lib.getExe pkgs.kitty}";
+            keybindings = {
+              # super + Enter = new default terminal window (kitty)
+              "$mod+Return" = "exec ${lib.getExe pkgs.kitty}";
 
-                # ctrl + shift + q = close window (canonicalized to Ctrl+Shift+q)
-                "Ctrl+Shift+q" = "kill";
+              # control + shift + q = close window
+              # i3 expects the modifier name "Control" (not "Ctrl")
+              "Control+Shift+q" = "kill";
 
-                # super + d = run rofi (application launcher)
-                "$mod+d" = "exec ${lib.getExe pkgs.rofi} -modi drun -show drun";
-              };
+              # super + d = run rofi (application launcher)
+              "$mod+d" = "exec ${lib.getExe pkgs.rofi} -modi drun -show drun";
+            };
           };
         };
       };
