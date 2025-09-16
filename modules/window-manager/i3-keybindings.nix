d0cae909d10ac7685c94f3de7ae086f67c49d66d
Bad3r <25513724+Bad3r@users.noreply.github.com>
2025-09-16 05:41:15 +0000

    fix(media/dotool): use lib.getExe' for dotoold to avoid abort-on-warn during evaluation

--- modules/window-manager/i3-keybindings.nix
+++ modules/window-manager/i3-keybindings.nix
@@ -1,29 +1,36 @@
 {
   # Home Manager i3 keybindings for common actions
   flake.homeManagerModules.gui =
-    { pkgs, lib, ... }:
+    {
+      pkgs,
+      lib,
+      config,
+      ...
+    }:
     {
       xsession = {
         enable = true;
         windowManager.i3 = {
           enable = true;
           config = {
-            # Use Super/Mod4 as the i3 modifier ("$mod")
+            # Use super as the i3 modifier ("$mod"). Under the hood $mod defaults to Mod4.
             modifier = lib.mkDefault "Mod4";
 
             # Keybindings requested
-            keybindings = {
-              # super + Enter = new default terminal window (kitty)
-              "$mod+Return" = "exec ${lib.getExe pkgs.kitty}";
+            keybindings =
+              let
+                normalizeMap = config.flake.lib.homeManager.i3.normalizeMap or (attrs: attrs);
+              in
+              normalizeMap {
+                # super + Enter = new default terminal window (kitty)
+                "$mod+Return" = "exec ${lib.getExe pkgs.kitty}";
 
-              # ctrl + shift + q = close window
-              # Accept both Control/Ctrl naming variants
-              "Control+Shift+q" = "kill";
-              "Ctrl+Shift+q" = "kill";
+                # ctrl + shift + q = close window (canonicalized to Ctrl+Shift+q)
+                "Ctrl+Shift+q" = "kill";
 
-              # super + d = run rofi (application launcher)
-              "$mod+d" = "exec ${lib.getExe pkgs.rofi} -modi drun -show drun";
-            };
+                # super + d = run rofi (application launcher)
+                "$mod+d" = "exec ${lib.getExe pkgs.rofi} -modi drun -show drun";
+              };
           };
         };
       };
