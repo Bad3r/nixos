275176bd06575a4bf7823a0530f52b473a7779b8
Bad3r <25513724+Bad3r@users.noreply.github.com>
2025-09-19 20:49:55 +0300

    feat(home): isolate sops-nix user state

--- modules/home-manager/base.nix
+++ modules/home-manager/base.nix
@@ -4,8 +4,10 @@
     args:
     let
       hmConfig = args.config;
+      hmLib = args.lib or lib;
       defaultHome = "/home/${config.flake.lib.meta.owner.username}";
       homeDir = hmConfig.home.homeDirectory or defaultHome;
+      sopsServiceHome = "${homeDir}/.local/share/sops-nix";
     in
     {
       home = {
@@ -18,5 +20,14 @@
       systemd.user.startServices = "sd-switch";
 
       sops.age.keyFile = lib.mkDefault "${homeDir}/.config/sops/age/keys.txt";
+
+      home.activation.ensureSopsServiceHome = hmLib.hm.dag.entryAfter [ "writeBoundary" ] ''
+        mkdir -p '${sopsServiceHome}'
+        chmod 700 '${sopsServiceHome}'
+      '';
+
+      systemd.user.services.sops-nix.Service.Environment = lib.mkForce [
+        "HOME=${sopsServiceHome}"
+      ];
     };
 }
