beefd30a3f977594247ad0f9363fabb8037c9695
Bad3r <25513724+Bad3r@users.noreply.github.com>
2025-09-20 17:32:31 +0300

    fix(pc): ensure allowed unfree option survives pc import

--- modules/pc/unfree-packages.nix
+++ modules/pc/unfree-packages.nix
@@ -7,8 +7,17 @@
   config.flake =
     let
       predicate = pkg: builtins.elem (lib.getName pkg) config.nixpkgs.allowedUnfreePackages;
+      optionModule =
+        { lib, ... }:
+        {
+          options.nixpkgs.allowedUnfreePackages = lib.mkOption {
+            type = lib.types.listOf lib.types.str;
+            default = [ ];
+          };
+        };
     in
     {
+      nixosModules.pc.imports = [ optionModule ];
       nixosModules.base.nixpkgs.config.allowUnfreePredicate = predicate;
 
       homeManagerModules.base = args: {
