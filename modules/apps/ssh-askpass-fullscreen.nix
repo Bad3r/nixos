1cef2a4b504245c668c85a02837409da3e8162f5
Bad3r <25513724+Bad3r@users.noreply.github.com>
2025-09-22 11:54:09 +0300

    Revert "fix(apps): expose ssh askpass module"
    This reverts commit 3263762d5e6bd76036d189c4834764c9b874496d.

--- modules/apps/ssh-askpass-fullscreen.nix
+++ modules/apps/ssh-askpass-fullscreen.nix
@@ -1,5 +1,5 @@
-let
-  module =
+{
+  flake.nixosModules.apps.ssh-askpass-fullscreen =
     { lib, pkgs, ... }:
     {
       environment.systemPackages = [ pkgs.ssh-askpass-fullscreen ];
@@ -18,15 +18,24 @@ let
         Defaults env_keep += "SSH_ASKPASS SSH_ASKPASS_REQUIRE DISPLAY WAYLAND_DISPLAY XAUTHORITY"
       '';
     };
-in
-{
-  flake = {
-    nixosModules = {
-      apps = {
-        ssh-askpass-fullscreen = module;
+
+  flake.nixosModules.base =
+    { lib, pkgs, ... }:
+    {
+      environment.systemPackages = [ pkgs.ssh-askpass-fullscreen ];
+
+      programs.ssh = {
+        enableAskPassword = true;
+        askPassword = lib.getExe pkgs.ssh-askpass-fullscreen;
       };
-      base = module;
-      "ssh-askpass-fullscreen" = module;
+
+      environment.variables.SSH_ASKPASS_REQUIRE = lib.mkDefault "force";
+
+      security.sudo.extraConfig = lib.mkAfter ''
+        Defaults env_keep += "SSH_ASKPASS SSH_ASKPASS_REQUIRE DISPLAY WAYLAND_DISPLAY XAUTHORITY"
+      '';
+      security.sudo-rs.extraConfig = lib.mkAfter ''
+        Defaults env_keep += "SSH_ASKPASS SSH_ASKPASS_REQUIRE DISPLAY WAYLAND_DISPLAY XAUTHORITY"
+      '';
     };
-  };
 }
