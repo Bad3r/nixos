3263762d5e6bd76036d189c4834764c9b874496d
Bad3r <25513724+Bad3r@users.noreply.github.com>
2025-09-22 11:47:32 +0300

    fix(apps): expose ssh askpass module

--- modules/apps/ssh-askpass-fullscreen.nix
+++ modules/apps/ssh-askpass-fullscreen.nix
@@ -1,41 +1,32 @@
+let
+  module =
+    { lib, pkgs, ... }:
+    {
+      environment.systemPackages = [ pkgs.ssh-askpass-fullscreen ];
+
+      programs.ssh = {
+        enableAskPassword = true;
+        askPassword = lib.getExe pkgs.ssh-askpass-fullscreen;
+      };
+
+      environment.variables.SSH_ASKPASS_REQUIRE = lib.mkDefault "force";
+
+      security.sudo.extraConfig = lib.mkAfter ''
+        Defaults env_keep += "SSH_ASKPASS SSH_ASKPASS_REQUIRE DISPLAY WAYLAND_DISPLAY XAUTHORITY"
+      '';
+      security.sudo-rs.extraConfig = lib.mkAfter ''
+        Defaults env_keep += "SSH_ASKPASS SSH_ASKPASS_REQUIRE DISPLAY WAYLAND_DISPLAY XAUTHORITY"
+      '';
+    };
+in
 {
-  flake.nixosModules.apps.ssh-askpass-fullscreen =
-    { lib, pkgs, ... }:
-    {
-      environment.systemPackages = [ pkgs.ssh-askpass-fullscreen ];
-
-      programs.ssh = {
-        enableAskPassword = true;
-        askPassword = lib.getExe pkgs.ssh-askpass-fullscreen;
+  flake = {
+    nixosModules = {
+      apps = {
+        ssh-askpass-fullscreen = module;
       };
-
-      environment.variables.SSH_ASKPASS_REQUIRE = lib.mkDefault "force";
-
-      security.sudo.extraConfig = lib.mkAfter ''
-        Defaults env_keep += "SSH_ASKPASS SSH_ASKPASS_REQUIRE DISPLAY WAYLAND_DISPLAY XAUTHORITY"
-      '';
-      security.sudo-rs.extraConfig = lib.mkAfter ''
-        Defaults env_keep += "SSH_ASKPASS SSH_ASKPASS_REQUIRE DISPLAY WAYLAND_DISPLAY XAUTHORITY"
-      '';
+      base = module;
+      "ssh-askpass-fullscreen" = module;
     };
-
-  flake.nixosModules.base =
-    { lib, pkgs, ... }:
-    {
-      environment.systemPackages = [ pkgs.ssh-askpass-fullscreen ];
-
-      programs.ssh = {
-        enableAskPassword = true;
-        askPassword = lib.getExe pkgs.ssh-askpass-fullscreen;
-      };
-
-      environment.variables.SSH_ASKPASS_REQUIRE = lib.mkDefault "force";
-
-      security.sudo.extraConfig = lib.mkAfter ''
-        Defaults env_keep += "SSH_ASKPASS SSH_ASKPASS_REQUIRE DISPLAY WAYLAND_DISPLAY XAUTHORITY"
-      '';
-      security.sudo-rs.extraConfig = lib.mkAfter ''
-        Defaults env_keep += "SSH_ASKPASS SSH_ASKPASS_REQUIRE DISPLAY WAYLAND_DISPLAY XAUTHORITY"
-      '';
-    };
+  };
 }
