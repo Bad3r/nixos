73784ef737bc76a8692fdf949f7c0c422586a58b
Bad3r <25513724+Bad3r@users.noreply.github.com>
2025-09-22 11:54:09 +0300

    Revert "chore(workstation): include ssh askpass"
    This reverts commit 0f1d0949c80cf113ece3dc54be3e9c0be9a40b6a.

--- modules/workstation.nix
+++ modules/workstation.nix
@@ -1,7 +1,4 @@
 { config, ... }:
 {
-  flake.nixosModules.workstation.imports = with config.flake.nixosModules; [
-    pc
-    ssh-askpass-fullscreen
-  ];
+  flake.nixosModules.workstation.imports = with config.flake.nixosModules; [ pc ];
 }
