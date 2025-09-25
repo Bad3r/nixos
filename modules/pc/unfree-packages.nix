6d8cb68d1132b49c276873123bbc3b35f0231b82
Bad3r <25513724+Bad3r@users.noreply.github.com>
2025-09-25 12:50:53 +0300

    fix(modules): allow coderabbit-cli unfree

--- modules/pc/unfree-packages.nix
+++ modules/pc/unfree-packages.nix
@@ -1,32 +1,55 @@
-{ lib, config, ... }:
+{
+  lib,
+  config,
+  inputs,
+  ...
+}:
+let
+  cfg = config;
+  predicate = pkg: builtins.elem (lib.getName pkg) cfg.nixpkgs.allowedUnfreePackages;
+in
 {
   options.nixpkgs.allowedUnfreePackages = lib.mkOption {
     type = lib.types.listOf lib.types.str;
     default = [ ];
   };
-  config.flake =
-    let
-      predicate = pkg: builtins.elem (lib.getName pkg) config.nixpkgs.allowedUnfreePackages;
-      optionModule =
-        { lib, ... }:
-        {
-          options.nixpkgs.allowedUnfreePackages = lib.mkOption {
-            type = lib.types.listOf lib.types.str;
-            default = [ ];
+  config = {
+    perSystem =
+      { system, ... }:
+      {
+        config = {
+          _module.args.pkgs = import inputs.nixpkgs {
+            inherit system;
+            overlays = cfg.nixpkgs.overlays or [ ];
+            config = (cfg.nixpkgs.config or { }) // {
+              allowUnfreePredicate = predicate;
+            };
           };
         };
-    in
-    {
-      nixosModules.pc.imports = [ optionModule ];
-      nixosModules.base.nixpkgs.config.allowUnfreePredicate = predicate;
-
-      homeManagerModules.base = args: {
-        nixpkgs.config = lib.mkIf (!(args.hasGlobalPkgs or false)) {
-          allowUnfreePredicate = predicate;
-        };
       };
+    flake =
+      let
+        optionModule =
+          { lib, ... }:
+          {
+            options.nixpkgs.allowedUnfreePackages = lib.mkOption {
+              type = lib.types.listOf lib.types.str;
+              default = [ ];
+            };
+          };
+      in
+      {
+        nixosModules.pc.imports = [ optionModule ];
+        nixosModules.base.nixpkgs.config.allowUnfreePredicate = predicate;
 
-      lib.meta.nixpkgs.allowedUnfreePackages = config.nixpkgs.allowedUnfreePackages;
-    };
+        homeManagerModules.base = args: {
+          nixpkgs.config = lib.mkIf (!(args.hasGlobalPkgs or false)) {
+            allowUnfreePredicate = predicate;
+          };
+        };
+
+        lib.meta.nixpkgs.allowedUnfreePackages = cfg.nixpkgs.allowedUnfreePackages;
+      };
+  };
 
 }
