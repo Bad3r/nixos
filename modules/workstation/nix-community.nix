c1c13f20061b2ea08b5a77ccaea23e89600351f3
Bad3r <25513724+Bad3r@users.noreply.github.com>
2025-09-29 15:29:19 +0300

    chore: update mirrors

--- modules/workstation/nix-community.nix
+++ modules/workstation/nix-community.nix
@@ -2,9 +2,6 @@
 let
   substitutersList = [
     "https://cache.garnix.io"
-    "https://mirrors.sjtug.sjtu.edu.cn/nix-channels/store"
-    "https://mirror.sjtu.edu.cn/nix-channels/store"
-    "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
     "https://cache.nixos.org/"
   ];
   trustedKeys = [
