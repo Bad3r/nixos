42c01ee7c6ff9b0e6e27d2adb453b379f92434c2
Bad3r <25513724+Bad3r@users.noreply.github.com>
2025-09-29 15:46:28 +0300

    feat(shell): enable command-not-found integration

--- modules/base/shell-config.nix
+++ modules/base/shell-config.nix
@@ -7,8 +7,15 @@
 
       environment.binsh = "${pkgs.dash}/bin/dash";
       environment.shellAliases.rm = "rip";
-      programs.zsh.enable = true;
-      programs.command-not-found.enable = false;
+      programs = {
+        zsh.enable = true;
+        command-not-found.enable = true;
+        nix-index = {
+          enable = true;
+          enableBashIntegration = false;
+          enableZshIntegration = false;
+        };
+      };
       users.mutableUsers = true;
       users.defaultUserShell = pkgs.zsh;
     };
