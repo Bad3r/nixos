ae51f7ae5fe0db19414c01cabab6aedd4dbfb4bb
Bad3r <25513724+Bad3r@users.noreply.github.com>
2025-10-10 11:00:04 +0300

    fix(system76): avoid recursion in virtualization module

--- modules/system76/virtualization.nix
+++ modules/system76/virtualization.nix
@@ -1,18 +1,19 @@
-_: {
+{ config, lib, ... }:
+let
+  virtualizationNamespace = lib.attrByPath [ "virtualization" ] { } (
+    (config.flake or { }).nixosModules or { }
+  );
+  libvirtModule = lib.attrByPath [ "libvirt" ] null virtualizationNamespace;
+  vmwareModule = lib.attrByPath [ "vmware" ] null virtualizationNamespace;
+  moduleImports = lib.filter (m: m != null) [
+    libvirtModule
+    vmwareModule
+  ];
+in
+{
   configurations.nixos.system76.module =
-    { config, lib, ... }:
-    let
-      virtualizationNamespace = lib.attrByPath [ "virtualization" ] { } (
-        config.flake.nixosModules or { }
-      );
-      libvirtModule = lib.attrByPath [ "libvirt" ] null virtualizationNamespace;
-      vmwareModule = lib.attrByPath [ "vmware" ] null virtualizationNamespace;
-      moduleImports = lib.filter (m: m != null) [
-        libvirtModule
-        vmwareModule
-      ];
-    in
+    { ... }:
     {
-      imports = lib.mkAfter moduleImports;
+      imports = moduleImports;
     };
 }
