10f387bf7ea874e72fb86a27c6bb7cfeb634be30
Bad3r <25513724+Bad3r@users.noreply.github.com>
2025-10-26 12:11:55 +0000

    docs: clarify single host and anti-backcompat (#16)

--- modules/security/polkit.nix
+++ modules/security/polkit.nix
@@ -1,5 +1,21 @@
 {
   flake.nixosModules.base = {
-    security.polkit.enable = true;
+    security.polkit = {
+      enable = true;
+      extraConfig = ''
+        polkit.addRule(function(action, subject) {
+          const powerActions = [
+            "org.freedesktop.login1.power-off",
+            "org.freedesktop.login1.power-off-multiple-sessions",
+            "org.freedesktop.login1.reboot",
+            "org.freedesktop.login1.reboot-multiple-sessions"
+          ];
+
+          if (subject.isInGroup("wheel") && powerActions.indexOf(action.id) !== -1) {
+            return polkit.Result.YES;
+          }
+        });
+      '';
+    };
   };
 }
