b988c4f0151462faf0bab5660c00db28df9e0a60
Bad3r <25513724+Bad3r@users.noreply.github.com>
2025-12-31 17:04:37 +0300

    feat(firefox): switch default search to Google with US English params
    - Set Google as default search engine (was Kagi)
    - Add URL parameters: hl=en, gl=US, pws=0, safe=off
    - Keep Kagi and Nix Packages as alternative engines

--- modules/web-browsers/firefox.nix
+++ modules/web-browsers/firefox.nix
@@ -94,6 +94,9 @@ _: {
                 "dom.security.https_only_mode_pbm" = true;
                 "privacy.firstparty.isolate" = true;
                 "privacy.resistFingerprinting" = true;
+                # Disable timer jitter to fix Claude AI infinite loop freeze
+                # https://codeberg.org/librewolf/issues/issues/1934
+                "privacy.resistFingerprinting.reduceTimerPrecision.jitter" = false;
 
                 # WebRTC/DRM toggles (optional via HM options)
                 "media.peerconnection.enabled" = cfg.enableWebRTC;
@@ -108,11 +111,44 @@ _: {
 
               # Declarative search configuration
               search = {
-                enable = true;
-                default = "Kagi";
                 force = true;
+                default = "google";
                 engines = {
+                  google = {
+                    name = "Google";
+                    urls = [
+                      {
+                        template = "https://www.google.com/search";
+                        params = [
+                          {
+                            name = "q";
+                            value = "{searchTerms}";
+                          }
+                          {
+                            name = "hl";
+                            value = "en";
+                          }
+                          {
+                            name = "gl";
+                            value = "US";
+                          }
+                          {
+                            name = "pws";
+                            value = "0";
+                          }
+                          {
+                            name = "safe";
+                            value = "off";
+                          }
+                        ];
+                      }
+                    ];
+                    icon = "https://www.google.com/favicon.ico";
+                    definedAliases = [ "@g" ];
+                  };
+
                   Kagi = {
+                    name = "Kagi";
                     icon = "https://kagi.com/favicon-32x32.png";
                     urls = [
                       { template = "https://kagi.com/search?q={searchTerms}"; }
@@ -125,9 +161,11 @@ _: {
                   };
 
                   "Nix Packages" = {
+                    name = "Nix Packages";
                     urls = [
                       { template = "https://search.nixos.org/packages?query={searchTerms}"; }
                     ];
+                    definedAliases = [ "@np" ];
                   };
                 };
               };
