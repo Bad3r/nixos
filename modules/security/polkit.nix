e8ff4d5e3d9ca491335ab5edfceca0fd422c7517
Bad3r <25513724+Bad3r@users.noreply.github.com>
2026-01-02 17:15:21 +0300

    feat(security): allow wheel group to manage systemd units without sudo
    Add polkit rule for org.freedesktop.systemd1.manage-units action,
    enabling systemctl start/stop/restart/reload without password prompt
    for wheel group members.

--- modules/security/polkit.nix
+++ modules/security/polkit.nix
@@ -2,32 +2,55 @@ let
   module =
     { config, lib, ... }:
     let
-      cfg = config.security.polkit.wheelPowerManagement;
+      powerCfg = config.security.polkit.wheelPowerManagement;
+      systemdCfg = config.security.polkit.wheelSystemdManagement;
     in
     {
-      options.security.polkit.wheelPowerManagement = {
-        enable = lib.mkEnableOption "password-less power management for wheel group members";
-      };
+      options.security.polkit = {
+        wheelPowerManagement = {
+          enable = lib.mkEnableOption "password-less power management for wheel group members";
+        };
 
-      config = lib.mkIf cfg.enable {
-        security.polkit = {
-          enable = true;
-          extraConfig = ''
-            polkit.addRule(function(action, subject) {
-              const powerActions = [
-                "org.freedesktop.login1.power-off",
-                "org.freedesktop.login1.power-off-multiple-sessions",
-                "org.freedesktop.login1.reboot",
-                "org.freedesktop.login1.reboot-multiple-sessions"
-              ];
-
-              if (subject.isInGroup("wheel") && powerActions.indexOf(action.id) !== -1) {
-                return polkit.Result.YES;
-              }
-            });
-          '';
+        wheelSystemdManagement = {
+          enable = lib.mkEnableOption "password-less systemctl start/stop/restart for wheel group members";
         };
       };
+
+      config = lib.mkMerge [
+        (lib.mkIf powerCfg.enable {
+          security.polkit = {
+            enable = true;
+            extraConfig = ''
+              polkit.addRule(function(action, subject) {
+                const powerActions = [
+                  "org.freedesktop.login1.power-off",
+                  "org.freedesktop.login1.power-off-multiple-sessions",
+                  "org.freedesktop.login1.reboot",
+                  "org.freedesktop.login1.reboot-multiple-sessions"
+                ];
+
+                if (subject.isInGroup("wheel") && powerActions.indexOf(action.id) !== -1) {
+                  return polkit.Result.YES;
+                }
+              });
+            '';
+          };
+        })
+
+        (lib.mkIf systemdCfg.enable {
+          security.polkit = {
+            enable = true;
+            extraConfig = ''
+              polkit.addRule(function(action, subject) {
+                if (action.id === "org.freedesktop.systemd1.manage-units" &&
+                    subject.isInGroup("wheel")) {
+                  return polkit.Result.YES;
+                }
+              });
+            '';
+          };
+        })
+      ];
     };
 in
 {
