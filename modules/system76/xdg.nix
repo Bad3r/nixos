e061159ca3a5d958b150581e0f19a25cbbb3f90c
Bad3r <25513724+Bad3r@users.noreply.github.com>
2026-01-17 21:05:17 +0300

    refactor: switch default browser to Floorp with i3 improvements
    Browser switch:
    - Update i3 browser command to use Floorp
    - Change browserClass default from firefox to floorp
    - Update mpv ytdl cookies to use floorp
    - Document browser defaults set by HM modules in system76/xdg.nix
    
    Window manager improvements:
    - Add Bitwarden floating window rules with positioned popup

--- modules/system76/xdg.nix
+++ modules/system76/xdg.nix
@@ -1,6 +1,10 @@
 {
   configurations.nixos.system76.module = {
-    xdg.menus.enable = true;
-    xdg.mime.enable = true;
+    xdg = {
+      menus.enable = true;
+      mime.enable = true;
+      # Browser defaults are set by HM browser modules (floorp.nix, etc.)
+      # User-level ~/.config/mimeapps.list always overrides system-level
+    };
   };
 }
