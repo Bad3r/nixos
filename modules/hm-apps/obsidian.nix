8cbf7fc9fc03bc91362797cc525a90f048946d07
Bad3r <25513724+Bad3r@users.noreply.github.com>
2026-01-20 16:42:07 +0300

    fix(obsidian): work around upstream HM activation script bug
    The home-manager obsidian module's activation script uses `install -m644`
    without the `-D` flag, which fails when ~/.config/obsidian/ doesn't exist
    on first activation. Add a pre-activation script to ensure the directory
    exists.

--- modules/hm-apps/obsidian.nix
+++ modules/hm-apps/obsidian.nix
@@ -16,6 +16,14 @@ _: {
           enable = true;
           # Package installed by NixOS module (not overridable here)
         };
+
+        # Workaround for upstream home-manager bug: the obsidian activation script
+        # uses `install -m644` without `-D`, which fails when ~/.config/obsidian/
+        # doesn't exist. This ensures the directory exists before that script runs.
+        # TODO: Remove when https://github.com/nix-community/home-manager/issues/XXXX is fixed
+        home.activation.ensureObsidianConfigDir = lib.hm.dag.entryBefore [ "obsidian" ] ''
+          mkdir -p "$HOME/.config/obsidian"
+        '';
       };
     };
 }
