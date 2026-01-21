b88048109884d15aab09000dcdd8d50ed53bba9f
Bad3r <25513724+Bad3r@users.noreply.github.com>
2026-01-21 17:03:13 +0300

    feat(zsh): source HM session variables in interactive shells
    Enables home.sessionVariables and home.sessionPath to work
    without requiring the HM zsh module.

--- modules/system76/zsh.nix
+++ modules/system76/zsh.nix
@@ -3,6 +3,9 @@
     programs.zsh = {
       enable = true;
       interactiveShellInit = ''
+        # Source Home Manager session variables (for home.sessionVariables, home.sessionPath)
+        source "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh"
+
         setopt NO_INTERACTIVE_COMMENTS
         alias nr='nix run nixpkgs#'
         alias ns='nix shell nixpkgs#'
