66e5096334e383c2734ba177ccfc5f9d1e7f363b
Bad3r <25513724+Bad3r@users.noreply.github.com>
2026-02-23 10:26:50 +0300

    feat(i3wm): split i3 startup module

--- modules/apps/i3wm/startup.nix
+++ modules/apps/i3wm/startup.nix
@@ -11,44 +11,66 @@
     let
       stylixColors = config.lib.stylix.colors.withHashtag or config.lib.stylix.colors;
       xfconfQuery = lib.getExe' pkgs.xfconf "xfconf-query";
-    in
-    {
-      config.xsession.windowManager.i3.config.startup = lib.mkAfter [
+      stickyKeyToggles = [
         {
-          command = ''
-            set_bool() {
-              ${xfconfQuery} -c "$1" -p "$2" -s false \
-                || ${xfconfQuery} -c "$1" -p "$2" -n -t bool -s false
-            }
-
-            set_bool accessibility /StickyKeys
-            set_bool accessibility /StickyKeys/LatchToLock
-            set_bool accessibility /StickyKeys/TwoKeysDisable
-            set_bool accessibility /SlowKeys
-            set_bool accessibility /BounceKeys
-            set_bool accessibility /MouseKeys
-            set_bool xfce4-session /general/StartAssistiveTechnologies
-          '';
-          always = true;
-          notification = false;
+          channel = "accessibility";
+          property = "/StickyKeys";
         }
         {
-          command = "${lib.getExe' pkgs.hsetroot "hsetroot"} -solid '${stylixColors.base00}'";
-          always = true;
-          notification = false;
+          channel = "accessibility";
+          property = "/StickyKeys/LatchToLock";
         }
-        # DPMS: Keep screens on for 1 hour (3600s) before standby/suspend/off
         {
-          command = "${pkgs.xset}/bin/xset dpms 3600 3600 3600";
-          always = true;
-          notification = false;
+          channel = "accessibility";
+          property = "/StickyKeys/TwoKeysDisable";
         }
-        # Screen saver: Blank after 1 hour (3600s)
         {
-          command = "${pkgs.xset}/bin/xset s 3600 3600";
-          always = true;
-          notification = false;
+          channel = "accessibility";
+          property = "/SlowKeys";
+        }
+        {
+          channel = "accessibility";
+          property = "/BounceKeys";
+        }
+        {
+          channel = "accessibility";
+          property = "/MouseKeys";
+        }
+        {
+          channel = "xfce4-session";
+          property = "/general/StartAssistiveTechnologies";
         }
       ];
+      mkSetFalseCommand = toggle: {
+        command =
+          "${xfconfQuery} -c ${lib.escapeShellArg toggle.channel} -p ${lib.escapeShellArg toggle.property} -s false"
+          + " || ${xfconfQuery} -c ${lib.escapeShellArg toggle.channel} -p ${lib.escapeShellArg toggle.property} -n -t bool -s false";
+        always = true;
+        notification = false;
+      };
+    in
+    {
+      config.xsession.windowManager.i3.config.startup = lib.mkAfter (
+        (map mkSetFalseCommand stickyKeyToggles)
+        ++ [
+          {
+            command = "${lib.getExe' pkgs.hsetroot "hsetroot"} -solid '${stylixColors.base00}'";
+            always = true;
+            notification = false;
+          }
+          # DPMS: Keep screens on for 1 hour (3600s) before standby/suspend/off
+          {
+            command = "${pkgs.xset}/bin/xset dpms 3600 3600 3600";
+            always = true;
+            notification = false;
+          }
+          # Screen saver: Blank after 1 hour (3600s)
+          {
+            command = "${pkgs.xset}/bin/xset s 3600 3600";
+            always = true;
+            notification = false;
+          }
+        ]
+      );
     };
 }
