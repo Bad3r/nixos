c3cf6252ab8907186094ebce7f9f4986bf0457ec
Bad3r <25513724+Bad3r@users.noreply.github.com>
2026-04-17 13:24:13 +0300

    chore(scripts): prefer sjtu bootstrap substituter

--- build.sh
+++ build.sh
@@ -200,8 +200,8 @@ fi
 # Bootstrap substituters for first build (before system has them configured)
 # Used for initial Determinate Nix setup or similar migrations
 BOOTSTRAP_SUBSTITUTERS=(
-  "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
-  #"https://mirror.sjtu.edu.cn/nix-channels/store"
+  #"https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
+  "https://mirror.sjtu.edu.cn/nix-channels/store"
   "https://mirrors.ustc.edu.cn/nix-channels/store"
   "https://cache.nixos.org"
   "https://cache.garnix.io"
