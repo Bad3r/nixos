917baf6855c66cb19e1104a10fb189efd9c72bd6
Bad3r <25513724+Bad3r@users.noreply.github.com>
2026-05-08 22:56:23 +0300

    chore(nix): update substituters

--- modules/system76/apps-enable.nix
+++ modules/system76/apps-enable.nix
@@ -268,7 +268,7 @@
       "node-cloudflare-sdk".extended.enable = lib.mkOverride 1100 false;
       nodejs_22.extended.enable = lib.mkOverride 1100 true;
       nodejs_24.extended.enable = lib.mkOverride 1100 true;
-      "nomachine-client".extended.enable = lib.mkOverride 1100 false;
+      "nomachine-client".extended.enable = lib.mkOverride 1100 true;
       normcap.extended.enable = lib.mkOverride 1100 true;
       nrm.extended.enable = lib.mkOverride 1100 true;
       nsxiv.extended.enable = lib.mkOverride 1100 true;
