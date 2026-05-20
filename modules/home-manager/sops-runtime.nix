0bd8215565d222fa6be2327d356119142470860e
Bad3r <github@unsigned.sh>
2026-05-20 09:54:16 +0000

    fix(home-manager): skip sops service when secrets are disabled (#268)

--- modules/home-manager/sops-runtime.nix
+++ modules/home-manager/sops-runtime.nix
@@ -1,6 +1,7 @@
 _: {
   flake.homeManagerModules.sopsRuntime =
     {
+      config,
       inputs,
       lib,
       metaOwner,
@@ -9,19 +10,26 @@ _: {
     let
       homeDirectory = "/home/${metaOwner.username}";
       sopsServiceHome = "${homeDirectory}/.local/share/sops-nix";
+      hasSopsMaterial = (config.sops.secrets or { }) != { } || (config.sops.templates or { }) != { };
     in
     {
       imports = [ inputs.sops-nix.homeManagerModules.sops ];
 
-      sops.age.keyFile = lib.mkDefault "${homeDirectory}/.config/sops/age/keys.txt";
+      config = lib.mkMerge [
+        {
+          sops.age.keyFile = lib.mkDefault "${homeDirectory}/.config/sops/age/keys.txt";
+        }
 
-      home.activation.ensureSopsServiceHome = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-        mkdir -p '${sopsServiceHome}'
-        chmod 700 '${sopsServiceHome}'
-      '';
+        (lib.mkIf hasSopsMaterial {
+          home.activation.ensureSopsServiceHome = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+            mkdir -p '${sopsServiceHome}'
+            chmod 700 '${sopsServiceHome}'
+          '';
 
-      systemd.user.services.sops-nix.Service.Environment = lib.mkForce [
-        "HOME=${sopsServiceHome}"
+          systemd.user.services.sops-nix.Service.Environment = lib.mkForce [
+            "HOME=${sopsServiceHome}"
+          ];
+        })
       ];
     };
 }
