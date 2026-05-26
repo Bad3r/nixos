1d2de42bc2c609a82372548dbde97bc8e0ff3cd4
Bad3r <github@unsigned.sh>
2026-05-27 02:35:31 +0300

    revert(browsers): restore Google sorry document exception
    The previous change moved the Google sorry handling into uBO medium-mode dynamic rules. That does not match the symptom being fixed: the www.google.com/sorry document itself needs an exception so Google reCAPTCHA challenges can load.
    
    Validation:
    
    - nix fmt -- modules/hm-apps/_gecko-extensions.nix
    
    - git diff --check
    
    - git diff --cached --check
    
    - nix-instantiate --parse modules/hm-apps/_gecko-extensions.nix

--- modules/hm-apps/_gecko-extensions.nix
+++ modules/hm-apps/_gecko-extensions.nix
@@ -301,9 +301,6 @@ let
     "accounts.google.com * 3p-frame noop"
     "myaccount.google.com * 3p-script noop"
     "myaccount.google.com * 3p-frame noop"
-    # Google ReCaptcha
-    "www.google.com/sorry * 3p-script noop"
-    "www.google.com/sorry * 3p-frame noop"
 
     # Microsoft 365
     "teams.cloud.microsoft * 3p-script noop"
@@ -439,6 +436,9 @@ in
 
       ! https://web.webex.com
       web.webex.com##.cookie-banner-body
+
+      ! https://www.google.com/sorry
+      @@||www.google.com/sorry^$document
     '';
 
     selectedFilterLists = lib.unique (
