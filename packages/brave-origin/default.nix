ec34ebc954e0d5c100f272bd05da0aee0c9c6d94
Bad3r <github@unsigned.sh>
2026-06-17 18:27:28 +0300

    chore(packages): run custom packages updater

--- packages/brave-origin/default.nix
+++ packages/brave-origin/default.nix
@@ -6,16 +6,16 @@
 
 let
   pname = "brave-origin";
-  version = "1.93.70";
+  version = "1.93.73";
 
   allArchives = {
     x86_64-linux = {
       url = "https://github.com/brave/brave-browser/releases/download/v${version}/brave-origin-nightly_${version}_amd64.deb";
-      hash = "sha256-Rr/XNnNqL/ohCN9l8OxU3atkqw+DCaZ/iNT5XBBZeec=";
+      hash = "sha256-B+y+4NCIm2jmYYJr7A57w5Q0i3pwJNYpcsyjvZuzO7s=";
     };
     aarch64-darwin = {
       url = "https://github.com/brave/brave-browser/releases/download/v${version}/brave-origin-v${version}-darwin-arm64.zip";
-      hash = "sha256-6ASFeerf2+jrw+R7U1tecL1KTo8+TdCKh+cizx3HOwA=";
+      hash = "sha256-rzRArAX71ritMSTp3N7wiKWGp/+TxuNdKs4epp14KIU=";
     };
   };
 
